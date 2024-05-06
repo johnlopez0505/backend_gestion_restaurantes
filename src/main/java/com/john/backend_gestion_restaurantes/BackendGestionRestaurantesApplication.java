d0cd5440060c2e81f468e6032d2a99a9a0a3b9eb
john-lopez <lopezcon1@hotmail.com>
2024-05-06 19:05:49 +0200

    creado proyecto

---
+++ src/main/java/com/john/backend_gestion_restaurantes/BackendGestionRestaurantesApplication.java
@@ -0,0 +1,13 @@
+package com.john.backend_gestion_restaurantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendGestionRestaurantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendGestionRestaurantesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/john/backend_gestion_restaurantes/BackendGestionRestaurantesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.john.backend_gestion_restaurantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendGestionRestaurantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
